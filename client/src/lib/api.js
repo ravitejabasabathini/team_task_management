73b9fa187949a4d4a73637c572a9e5e160884d0d
Srisneha <Srisnehabasabathini@gmail.com>
2026-05-10 10:37:51 +0530

    connected frontend to backend

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 })
 
 api.interceptors.request.use((config) => {
@@ -11,4 +11,3 @@ api.interceptors.request.use((config) => {
 })
 
 export default api
-
